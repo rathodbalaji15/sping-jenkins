793ee64291d270a706cd47afa2ce9558a4b228c5
Balaji Rathod <rathodbalaji123@gmail.com>
2025-11-22 12:05:50 +0530

    spring boot app for jenkins build

---
+++ spring-app-jenkins/src/main/java/com/example/SpringAppJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppJenkinsApplication.class, args);
+	}
+
+}
---
+++ spring-app-jenkins/src/main/java/com/example/controller/DemoController.java
@@ -0,0 +1,12 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping("/hello")
+    public String say(){
+        return "Hey Hello ...!!";
+    }
+}
